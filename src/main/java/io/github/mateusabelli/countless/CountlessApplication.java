9d9da9250ac70bdc979fa8f34b3e94c065cb6962
Mateus Abelli <mateusabelli@gmail.com>
2023-05-12 16:23:45 -0300

    Initialize Spring Boot

---
+++ src/main/java/io/github/mateusabelli/countless/CountlessApplication.java
@@ -0,0 +1,13 @@
+package io.github.mateusabelli.countless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountlessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountlessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/mateusabelli/countless/CountlessApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.mateusabelli.countless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountlessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
